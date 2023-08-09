e74ee07addd096964510b9c37af422308f7777de
Arden Ranta <arranta20@gmail.com>
2023-08-09 16:57:45 -0700

    Add second challenge

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+// from an array of mixed-case strings, print all strings in lowercase
+// need to create an array of strings with both upper- and lowercase letters
+// need to code a loop to catch each string in the array, have a built-in method for changing the case
+// need to print all lowercase strings to the console
+
+var strings = ["Taco", "leAF", "BaNjO", "sYLLABUS"];
+  for (var i = 0; i < strings.length; i++) {
+    console.log(strings[i].toLowerCase());
+    }
